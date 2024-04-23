53f4f4821cf6fccb6aaeec7480c9aaba9c7c476f
= <=>
2024-04-23 10:57:37 +0200

    Tema11

---
+++ src/ej3/LecturaEscritura.java
@@ -0,0 +1,44 @@
+package ej3;
+import java.io.*;
+import java.util.*;
+
+public class LecturaEscritura {
+
+	public static void main(String[] args) {
+		//Primera parte
+		String rutaFichero=".\\src\\ejemplos\\prueba.txt";
+		System.out.println("Contenido del fichero: ");
+		ArrayList<String> lista = new ArrayList<>();
+		
+		File fichero = new File(rutaFichero);
+		try(BufferedReader br = new BufferedReader(new FileReader(fichero))){
+			String linea;
+			while((linea = br.readLine()) !=null) {
+				System.out.println(linea);
+				lista.add(linea);
+			}
+		}catch(IOException e){
+			e.printStackTrace();
+		}
+		//Pedir datos
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Introduzca el dato que desea introducir: ");
+		String dato = sc.nextLine();
+		añadirDato(lista, dato);
+		imprimirLista(lista);
+		
+	
+		sc.close();
+	}
+	public static void añadirDato (ArrayList<String> lista,String dato) {
+		lista.add(dato);		
+	}
+	public static void imprimirLista(ArrayList<String> lista) {
+		Iterator<String> imprimir = lista.iterator();
+		while(imprimir.hasNext()) {	
+			imprimir.next();			
+		}
+		System.out.println(lista);
+	}
+
+}
---
+++ src/ejemplos/Ejemplo3.java
@@ -0,0 +1,61 @@
+package ejemplos;
+import java.io.*;
+import java.util.Scanner;
+
+/**
+ * Explicacion de la escritura de información
+ */
+public class Ejemplo3 {
+    /**
+     * Metodo que ejecuta el codigo
+     * @param args Los argumentos de la linea de comandos
+     */
+    public static void main(String[] args) throws IOException {
+        Scanner sc =new Scanner(System.in);
+        File fichero1 = new File(".\\src\\ejemplos\\prueba.txt");
+        //Compruebo que el directorio existe
+        if (!fichero1.exists()){
+            System.out.println("El archivo no exite!!!");
+            System.out.println("¿Quiere crear un nuevo fichero de texto?"+"\nPulse Y para crear el archivo");
+            String  respuesta= sc.nextLine();
+            if(respuesta.equalsIgnoreCase("Y")) {
+                System.out.println("Nombre del nuevo fichero: ");
+                String nombreFichero = sc.nextLine();
+                //Crear un nuevo fichero con un nombre introducido por el usuario
+                File ficheroUser = new File(".\\src\\ejemplos\\" + nombreFichero);
+                try {
+                    ficheroUser.createNewFile();
+                    ficheroUser.getAbsoluteFile();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }//Fin try-catch
+                System.out.println("El archivo se ha creado correctamente");
+            }else{
+                System.out.println("El programa se cerrara");
+            }//Fin else
+
+        }else{
+            try {
+                BufferedReader br = new BufferedReader(new FileReader(".\\src\\ejemplos\\prueba.txt"));
+                BufferedWriter bw = new BufferedWriter(new FileWriter(".\\src\\ejemplos\\prueba.txt"));
+                //Escribimos en el fichero
+                bw.write("Linea 1");
+                System.out.println("Escriba su linea 1");
+                String escritura= sc.nextLine();
+                bw.write(escritura);
+                bw.flush();
+                bw.newLine();
+                bw.write("Linea 2");
+                System.out.println("Escriba su linea 2");
+               escritura= sc.nextLine();
+                bw.write(escritura);
+                bw.flush();
+                bw.close();
+                br.close();
+            }catch (Exception e){
+                e.printStackTrace();
+            }//Fin try-catch
+        }//Fin if-else
+        sc.close();
+    }//Fin main
+}//Fin del main
